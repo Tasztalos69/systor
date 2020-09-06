26ea4d6d35f98a6566da0e59b5e15042d54623ec
ApOszTroF <tasztalos69@gmail.com>
2020-09-06 12:48:09 +0200

    Correct postinstall message

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,3 @@
 console.log(
-  '\u001b[1m\u001b[33mIf you have trouble with viewing the world map, please install the font located in /assets.\u001b[39m\u001b[22m',
+  "\u001b[1m\u001b[33mIf you're on windows, please follow these instructions: https://bit.ly/systorwin \u001b[39m\u001b[22m",
 );
